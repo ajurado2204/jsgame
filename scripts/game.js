bbb9198ea151a4a225a6a72ea27ff84f8c44907e
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2015-11-15 20:52:48 -0500

    Edited JS file to disable click after game is over

--- scripts/game.js
+++ scripts/game.js
@@ -1,19 +1,30 @@
+// Global Variables
+
   var decTimer;
+  var secondsLeft = 20;
+  var count = 0;
+  var img = document.getElementsByClassName("character");
 
-  
-    document.getElementById("start").addEventListener("click", startTimer);
-
-  
-
-
+// Function to Start Timer and allow image to be clicked
 
   function startTimer(){
      decTimer = setInterval(myTimer, 1000);
+     for (var i = 0; i < img.length; i++) {
+      img[i].setAttribute("data-clickable", "true");
+    }
   };
 
   document.getElementById("start").addEventListener("click", startTimer);
 
-  var secondsLeft = 20;
+// Function to Reload the page and to start game again
+
+  function reloadPage(){
+      window.location.reload();
+  };
+
+  document.getElementById("restart").addEventListener("click", reloadPage);
+
+// Function for count down, alert user score and prevent future clicks after game is over
 
   function myTimer(){
     if(secondsLeft > 0){
@@ -22,38 +33,30 @@
         document.getElementById("timeLine").innerHTML = "Your time is up!";
         document.getElementById("timeLine").className = "change";
         alert("Congratulations! You clicked " + count + " characters!");
+        for (var i = 0; i < img.length; i++) {
+         img[i].setAttribute("data-clickable", "false");
+        };
         clearInterval(decTimer);
     };
   }; 
-  
-  var count = 0;
-
-    function countClicks(){      
-      if (this.getAttribute('data-clickable') == 'true'){
-        count++;
-        this.setAttribute('data-clickable', 'false');
-      }
-      document.getElementById("clickTotal").innerHTML = count;
-    };
-
-
-  
-  
  
+// Function to count number of clicks
 
-  // add data to all set to on and then after click turn to all 
-  // at 0 zecond set all to off
+  function countClicks(){      
+    if (this.getAttribute('data-clickable') == 'true'){
+      count++;
+      this.setAttribute('data-clickable', 'false');
+      this.className = "clicked";
+      document.getElementById("clickTotal").innerHTML = count;
+      
+    };
+  };
 
+// Loop adding the onclicks to images 
 
-
-for (i = 0; i < 20; i++){
-  document.getElementsByClassName("character")[i].addEventListener("click", countClicks);
-}
-  
-
-  
-
-
+  for (i = 0; i < 20; i++){
+    document.getElementsByClassName("character")[i].addEventListener("click", countClicks);
+  };
 
 
   
